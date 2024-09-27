54929a54e16850b6ceeceb218679b642e425541b
GeraldaGultom <geraldagultom@gmail.com>
2024-09-27 11:29:39 +0700

    Kami sudah menyelesaikan tugas

--- T02.java
+++ T02.java
@@ -54,4 +54,3 @@ public class T02 {
         System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunTerbit + "|" + penerbit + "|" + formatBukuElektronik + "|" + hargaPembelian + "|" + minimumMargin + "|" + stok + "|" + rating + "|" + kategori);
     }
 }
-
